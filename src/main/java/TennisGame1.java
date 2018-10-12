cfa117261cae1224215e0d937a496dd1c1426e6c
camacho20 <oweimar@hotmail.com>
2018-10-12 16:36:16 -0500

    Somos Oweimar y Valentina Uniamazonia

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,10 +1,12 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_puntaje1 = 0;
-    private int m_puntaje2 = 0;
+    private int puntaje_jugador1 = 0;
+    private int puntaje_jugador2 = 0;
     private String nombreJugador1;
     private String nombreJugador2;
+    String puntaje = "";
+    int PuntajeTemporal=0;
 
     public TennisGame1(String nombreJugador1, String nombreJugador2) {
         this.nombreJugador1 = nombreJugador1;
@@ -13,64 +15,77 @@ public class TennisGame1 implements TennisGame {
 
     public void ganoPunto(String nombreJugador) {
         if (nombreJugador == "jugador1")
-            m_puntaje1 += 1;
+            puntaje_jugador1 += 1;
         else
-            m_puntaje2 += 1;
+            puntaje_jugador2 += 1;
     }
 
     public String getPuntaje() {
-        String puntaje = "";
-        int tempPuntaje=0;
-        if (m_puntaje1 == m_puntaje2)
+       
+        if (puntaje_jugador1 == puntaje_jugador2)
         {
-            switch (m_puntaje1)
-            {
-                case 0:
-                        puntaje = "Amor-Todos";
-                    break;
-                case 1:
-                        puntaje = "Quince-Todos";
-                    break;
-                case 2:
-                        puntaje = "Treinta-Todos";
-                    break;
-                default:
-                        puntaje = "Deuce";
-                    break;
-                
-            }
+            CompararPuntaje(); 
         }
-        else if (m_puntaje1 >=4 || m_puntaje2 >=4)
+        else if (puntaje_jugador1 >=4 || puntaje_jugador2 >=4)
         {
-            int minusResult = m_puntaje1 - m_puntaje2;
-            if (minusResult==1) puntaje ="Ventaja jugador1";
-            else if (minusResult ==-1) puntaje ="Ventaja jugador2";
-            else if (minusResult>=2) puntaje = "Gana jugador1";
-            else puntaje ="Gana jugador2";
+            DiferenciaEntreJugadores(); 
         }
         else
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempPuntaje = m_puntaje1;
-                else { puntaje+="-"; tempPuntaje = m_puntaje2;}
-                switch(tempPuntaje)
-                {
-                    case 0:
-                        puntaje+="Amor";
-                        break;
-                    case 1:
-                        puntaje+="Quince";
-                        break;
-                    case 2:
-                        puntaje+="Treinta";
-                        break;
-                    case 3:
-                        puntaje+="Cuarenta";
-                        break;
-                }
+                if (i==1) PuntajeTemporal = puntaje_jugador1;
+                else { puntaje+="-"; PuntajeTemporal = puntaje_jugador2;}
+                CompararPuntajeTemporal(); 
             }
         }
         return puntaje;
+    }
+    
+    public String DiferenciaEntreJugadores() {
+    	int DiferenciaEnResultados = puntaje_jugador1 - puntaje_jugador2;
+        if (DiferenciaEnResultados==1) puntaje ="Ventaja jugador1";
+        else if (DiferenciaEnResultados ==-1) puntaje ="Ventaja jugador2";
+        else if (DiferenciaEnResultados>=2) puntaje = "Gana jugador1";
+        else puntaje ="Gana jugador2";
+    	return puntaje; 
+    }
+    
+    public String CompararPuntaje() {
+    	  switch (puntaje_jugador1) {
+                case 0:
+                    puntaje = "Amor-Todos";
+                    break;
+                case 1:
+                    puntaje = "Quince-Todos";
+                    break;
+                case 2:
+                    puntaje = "Treinta-Todos";
+                    break;
+                default:
+                    puntaje = "Deuce";
+                    break;
+
+            }
+        return puntaje; 
+    }
+    
+    public String CompararPuntajeTemporal(){
+    	  switch(PuntajeTemporal)
+          {
+              case 0:
+                  puntaje+="Amor";
+                  break;
+              case 1:
+                  puntaje+="Quince";
+                  break;
+              case 2:
+                  puntaje+="Treinta";
+                  break;
+              case 3:
+                  puntaje+="Cuarenta";
+                  break;
+          }
+    	  return puntaje; 
     }
 }
